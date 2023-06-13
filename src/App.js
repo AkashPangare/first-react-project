04e010756f1501f158271ba611d03c6e333af07c
Akash Pangare <apangare70@gmail.com>
2023-06-13 16:18:01 +0530

    css

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 
 function App() {
   
-  let str = `Hello Universe!!!`;
-  let pi = 3.141159;
-  let user = {id: 1,Name: "Akash"};
-  
+  let style ={background:"green",color:"white"};
+
   return(
     <>
-    <h1>Hello Universe</h1>
-    <h1>pi {pi}</h1>
-    <h1>{str}</h1>
-    <h1>Id:{user.id},Name:{user.Name}</h1>
+    <h1 style ={style}>Hello World</h1>
+    <h1 style={{background:"blue",color:"black"}}>Hello World</h1>
     </>
-  )
+
+  );
 }
 
 export default App;
