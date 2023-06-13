7538eec686c7b07256b6a0ba3213e49093ead0bc
Akash Pangare <apangare70@gmail.com>
2023-06-13 15:37:34 +0530

    paragraph

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   return (
-  <>
-  <h1>Hello World</h1>
-  </>
-  );
-    
-    
+  
+  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+  mollitia exercitationem veritatis harum illum accusantium earum laborum
+  atque natus a qui repudiandae sapiente quae similique, impedit dolor, eius
+  facere laboriosam.</h1>
+  
+  
+  );  
 }
 
 export default App;
