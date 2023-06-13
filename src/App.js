310c3f41beaa9f5ea9b7fd00d1705ac275242453
Akash Pangare <apangare70@gmail.com>
2023-06-13 16:21:26 +0530

    css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   
-  let style ={background:"green",color:"white"};
+  
 
   return(
     <>
-    <h1 style ={style}>Hello World</h1>
+    <h1 class="classroom">Hello World</h1>
     <h1 style={{background:"blue",color:"black"}}>Hello World</h1>
     </>
 
