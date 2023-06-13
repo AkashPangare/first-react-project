7456f8f632faa3d2f21d3959c260ffb8be71e437
Akash Pangare <apangare70@gmail.com>
2023-06-13 15:41:08 +0530

    variable

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-  
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-  mollitia exercitationem veritatis harum illum accusantium earum laborum
-  atque natus a qui repudiandae sapiente quae similique, impedit dolor, eius
-  facere laboriosam.</h1>
-  
-  
-  );  
+ let first = <h1>Hello Universe</h1>;
+return first;
 }
 
 export default App;
