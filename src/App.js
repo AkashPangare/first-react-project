a39debc6dd950a03f567422401f626406487fef4
Akash Pangare <apangare70@gmail.com>
2023-06-13 15:43:20 +0530

    2 variable

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
- let first = <h1>Hello Universe</h1>;
-return first;
+  let jsx1 = <h1>Good Morning</h1>;
+  let jsx2 = <h1>Good Night</h1>;
+
+  return false ? jsx1 : jsx2;
 }
 
 export default App;
