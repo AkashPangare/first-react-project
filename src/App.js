4256b8e7ecb07e4307ac9220b90071f4e0699899
Akash Pangare <apangare70@gmail.com>
2023-06-13 16:28:40 +0530

    paragraph

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
-
+//import './App.css';
 function App() {
-  
-  
-
-  return(
+  return (
     <>
-    <h1 class="classroom">Hello World</h1>
-    <h1 style={{background:"blue",color:"black"}}>Hello World</h1>
-    </>
+      <h1 className="bg-primary text-light p-3">My Project</h1>
 
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        laborum minima doloremque tempora in alias delectus voluptatum ipsum.
+        Deleniti optio ad rem provident esse, veritatis ipsam incidunt. Illum,
+        explicabo omnis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Provident esse tenetur blanditiis voluptatibus ex ab, ad in harum
+        nostrum ipsam quaerat quia dolore suscipit id consequatur quis delectus
+        neque animi?
+      </div>
+    </>
   );
 }
 
