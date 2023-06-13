5fa02487e0c54946b4bfd8b878799e75549c8101
Akash Pangare <apangare70@gmail.com>
2023-06-13 16:43:16 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 //import './App.css';
+import DataBlock from "./DataBlock";
+
 function App() {
   return (
     <>
@@ -13,23 +15,6 @@ function App() {
   );
 }
 
-function DataBlock() {
-  return (
-    <>
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nihil
-        adipisci ducimus quisquam a rerum accusantium consequuntur. Quasi itaque
-        qui consectetur, culpa iste nam in quibusdam quisquam doloribus, dicta
-        perspiciatis accusamus praesentium! Quos accusantium explicabo alias
-        perspiciatis voluptates totam consectetur laboriosam voluptas cumque,
-        amet quasi vero nostrum quibusdam ad in mollitia soluta! Voluptatem,
-        porro quo ut in et corporis sunt facere accusantium soluta nisi
-        reprehenderit repellat, minus error, veniam ipsa quasi amet. Numquam hic
-        doloribus, aspernatur saepe .
-      </div>
-    </>
-  );
-}
+
 
 export default App;
