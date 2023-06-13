6b250ad883552e4a50aef964e8b6b2e11e231ede
Akash Pangare <apangare70@gmail.com>
2023-06-13 16:07:23 +0530

    object

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 
 function App() {
   
-  let str ="Hello World";
-  let pi = 3.14;
-  let active = true;
+  let str = `Hello Universe!!!`;
+  let pi = 3.141159;
+  let user = {id: 1,Name: "Akash"};
   
   return(
     <>
     <h1>Hello Universe</h1>
     <h1>pi {pi}</h1>
     <h1>{str}</h1>
-    <h1>{active ? "good morning" : "good night"}</h1>
-    {active ? "Good morning": "Good night"};
-    {active && <h1>Good Morning</h1>}
+    <h1>Id:{user.id},Name:{user.Name}</h1>
     </>
   )
 }
